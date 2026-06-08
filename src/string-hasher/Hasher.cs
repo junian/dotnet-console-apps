bd8bee25befe1f3dfb718b19019d06123b471c69
Junian Triajianto <author@junian.dev>
2026-06-08 22:40:39 +0700

    Implement string hashing functionality with MD5, SHA1, SHA256, SHA384, and SHA512 algorithms

---
+++ src/string-hasher/Hasher.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace StringHasher;
+
+public static class Hasher
+{
+    // We create our functions here
+    private static string GenerateHashString(HashAlgorithm algo, string text)
+    {
+        // Compute hash from text parameter
+        algo.ComputeHash(Encoding.UTF8.GetBytes(text));
+
+        // Get has value in array of bytes
+        var result = algo.Hash;
+
+        // Return as hexadecimal string
+        return string.Join(
+            string.Empty,
+            result.Select(x => x.ToString("x2")));
+    }
+
+    public static string MD5(string text)
+    {
+        var result = default(string);
+
+        using (var algo = new MD5CryptoServiceProvider())
+        {
+            result = GenerateHashString(algo, text);
+        }
+
+        return result;
+    }
+
+    public static string SHA1(string text)
+    {
+        var result = default(string);
+
+        using (var algo = new SHA1Managed())
+        {
+            result = GenerateHashString(algo, text);
+        }
+
+        return result;
+    }
+
+    public static string SHA256(string text)
+    {
+        var result = default(string);
+
+        using (var algo = new SHA256Managed())
+        {
+            result = GenerateHashString(algo, text);
+        }
+
+        return result;
+    }
+
+    public static string SHA384(string text)
+    {
+        var result = default(string);
+
+        using (var algo = new SHA384Managed())
+        {
+            result = GenerateHashString(algo, text);
+        }
+
+        return result;
+    }
+
+    public static string SHA512(string text)
+    {
+        var result = default(string);
+
+        using (var algo = new SHA512Managed())
+        {
+            result = GenerateHashString(algo, text);
+        }
+
+        return result;
+    }
+}
